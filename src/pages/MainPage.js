c7f0185c292d8579b84c2baf4f7a36afe2e17ae6
K-MHOON <azmria117@gmail.com>
2024-04-10 21:21:47 +0900

    feat : 메인 화면 컴포넌트 구현

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import BasicLayout from "../layouts/BasicLayout";
+import SearchBar from "../components/menus/SearchBar";
+import { Button, Container, Row, Col } from "react-bootstrap";
+import AuctionSlider from "../components/menus/AuctionSlider";
 
 const MainPage = () => {
     return (
         <BasicLayout>
-            <div className="text-3xl">
-                <div>Main Page</div>
-            </div>
+            <SearchBar />
+            <AuctionSlider />
+            <Row>
+                <Col
+                    style={{
+                        display: "flex",
+                        justifyContent: "end",
+                    }}
+                >
+                    <Button variant="primary">경매 시작하기</Button>
+                </Col>
+            </Row>
         </BasicLayout>
     );
 };
