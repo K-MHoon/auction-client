4c555973f3aeae4ac3c40c425c008078d93c621c
K-MHOON <azmria117@gmail.com>
2024-04-13 18:45:35 +0900

    리코일, 리액트쿼리 라이브러리 추가 및 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import root from "./router/root";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
-    return <RouterProvider router={root} />;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={root} />
+            <ReactQueryDevtools initialIsOpen={true} />
+        </QueryClientProvider>
+    );
 }
 
 export default App;
