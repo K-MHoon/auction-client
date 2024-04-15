8cd5efd09e6e115a09c555162f64c26ce9b33bcf
K-MHOON <azmria117@gmail.com>
2024-04-15 21:27:23 +0900

    fix : 로그인 페이지 수정

--- src/pages/member/LoginPage.js
+++ src/pages/member/LoginPage.js
@@ -1,32 +1,19 @@
 import React from "react";
-import LoginComponent from "../../components/members/LoginComponent";
-import "./LoginPage.css";
-import { Image } from "react-bootstrap";
-import background from "../../images/shiba_auction.png";
+import LoginComponent from "./../../components/members/LoginComponent";
 
 const LoginPage = () => {
     return (
-        <>
-            <Image
-                src={background}
-                style={{
-                    width: "100%",
-                    height: "100vh",
-                    position: "absolute",
-                }}
-            ></Image>
-            <div
-                style={{
-                    height: "100vh",
-                    position: "relative",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    display: "flex",
-                }}
-            >
-                <LoginComponent />
-            </div>
-        </>
+        <div
+            style={{
+                height: "100vh",
+                position: "relative",
+                alignItems: "center",
+                justifyContent: "center",
+                display: "flex",
+            }}
+        >
+            <LoginComponent />
+        </div>
     );
 };
 
