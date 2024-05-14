cd3187aff3f9999409e99e06265b8266207e4a74
K-MHOON <azmria117@gmail.com>
2024-05-14 21:12:17 +0900

    feat : ItemAPI 추가

--- src/api/itemApi.js
+++ src/api/itemApi.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import jwtAxios from "../util/jwtUtil";
 import { API_SERVER_HOST } from "./info";
 
@@ -7,5 +8,29 @@ export const itemAdd = async (item) => {
     const header = { headers: { "Content-Type": "multipart/form-data" } };
 
     const res = await jwtAxios.post(`${host}/item`, item, header);
+    return res.data;
+};
+
+export const itemDelete = async (seqList) => {
+    const res = await jwtAxios.delete(`${host}/item`, {
+        data: {
+            seqList: seqList,
+        },
+    });
+    return res.data;
+};
+
+export const itemGet = async (seq) => {
+    const res = await jwtAxios.get(`${host}/item/${seq}`);
+    return res.data;
+};
+
+export const fileGet = async (fileName) => {
+    const res = await axios.get(
+        `${API_SERVER_HOST}/api/view/item/${fileName}`,
+        {
+            responseType: "blob",
+        }
+    );
     return res.data;
 };
