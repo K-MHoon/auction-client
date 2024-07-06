bf521b1c2fc51d203d1718b7536afb86ad7c756f
K-MHOON <azmria117@gmail.com>
2024-07-06 16:40:45 +0900

    feat : AuctionSlider 소켓 연결

--- src/components/menus/AuctionSlider.js
+++ src/components/menus/AuctionSlider.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Slider from "react-slick";
 import "./AuctionSlider.css";
 import { useQuery } from "@tanstack/react-query";
@@ -7,6 +7,8 @@ import FetchingModal from "../common/FetchingModal";
 import { Card, Stack } from "react-bootstrap";
 import { API_SERVER_HOST } from "../../api/info";
 import { KRW } from "../common/CommonFunc";
+import { Client } from "@stomp/stompjs";
+import { jwtHeader } from "../../util/jwtUtil";
 
 const initState = {
     sequence: 0,
@@ -19,6 +21,36 @@ const initState = {
 };
 
 const AuctionSlider = () => {
+    const [data, setData] = useState([initState]);
+    const client = useRef({});
+
+    const disconnect = () => {
+        client.current.deactivate();
+        console.log("연결이 종료되었습니다.");
+    };
+
+    useEffect(() => {
+        const connect = () => {
+            client.current = new Client({
+                connectHeaders: {
+                    Authorization: jwtHeader,
+                },
+                brokerURL: "ws://localhost:8080/api/user/ws",
+                onConnect: () => {
+                    console.log("연결 성공");
+                },
+                onStompError: (frame) => {
+                    console.error(frame);
+                },
+            });
+            client.current.activate();
+        };
+
+        connect();
+
+        return () => disconnect();
+    }, []);
+
     const query = useQuery({
         queryKey: ["auctionSlider"],
         queryFn: () => auctionSliderListGet(),
@@ -26,7 +58,10 @@ const AuctionSlider = () => {
         retry: 1,
     });
 
-    const data = query.data || [initState];
+    useEffect(() => {
+        if (!query) return;
+        setData(query.data);
+    }, [query.isSuccess]);
 
     const settings = {
         infinite: true,
