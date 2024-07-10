8cf6181ab089190aaef9832c4f76e595ce05fcf8
K-MHOON <azmria117@gmail.com>
2024-07-10 20:34:57 +0900

    feat : 경매 시스템 경매 정보, 아이템 정보 분리

--- src/components/auction/ProcessComponent.js
+++ src/components/auction/ProcessComponent.js
@@ -1,16 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Col, Form, Row, Image } from "react-bootstrap";
-import coupon from "../../images/shiba_coupon.png";
 import FormTextBox from "../common/FormTextBox";
+import { useQuery } from "@tanstack/react-query";
+import { auctionGet } from "../../api/auctionApi";
+import { API_SERVER_HOST } from "../../api/info";
+import FetchingModal from "../common/FetchingModal";
+import useCustomSocket from "../../hooks/useCustomSocket";
+import { KRW } from "./../common/CommonFunc";
+
+const itemState = {
+    sequence: 0,
+    name: "",
+    description: "",
+    type: "",
+    itemImageList: [{ fileName: "default.png", ord: 0 }],
+};
+
+const initState = {
+    sequence: 0,
+    title: "",
+    minPrice: 0,
+    price: 0,
+    description: "",
+    status: "",
+    soldTime: "",
+    item: itemState,
+    endTime: "",
+    auctionType: "",
+    auctionImageList: [{ fileName: "default.png" }],
+    seller: "",
+    buyer: "",
+    startTime: "",
+    maxParticipantCount: 0,
+};
 
 const ProcessComponent = ({ pno }) => {
+    const { client } = useCustomSocket();
+
+    const query = useQuery({
+        queryKey: ["auction", pno],
+        queryFn: () => auctionGet(pno),
+        staleTime: 1000 * 10 * 60,
+        retry: 1,
+    });
+
+    const data = query.data || initState;
+
     return (
         <>
+            <FetchingModal flag={query.isFetching} />
             <Row
                 className="fw-bold fs-3 position-absolute"
                 style={{ marginTop: "-30px" }}
             >
-                현재 참가자 수 : 8
+                현재 참가자 수 : 8 / {data.maxParticipantCount}
             </Row>
             <Row
                 className="justify-content-md-end"
@@ -33,13 +76,105 @@ const ProcessComponent = ({ pno }) => {
                     05:22
                 </Col>
             </Row>
-            <Row className="justify-content-md-center fw-bold border border-secondary border-5 rounded-5">
+
+            <Row md={8} className="p-3 text-center justify-content-md-center">
+                <Form.Label
+                    style={{
+                        textAlign: "center",
+                        marginBottom: 20,
+                        fontSize: "30px",
+                        fontWeight: "bold",
+                    }}
+                >
+                    경매 정보(Auction)
+                </Form.Label>
+            </Row>
+            <Row
+                style={{
+                    border: "5px solid black",
+                    borderRadius: "20px",
+                    padding: "30px",
+                }}
+            >
                 <Col
                     md="auto"
                     style={{ alignItems: "center", display: "flex" }}
                 >
-                    <Image width={250} height={300} src={coupon}></Image>
+                    <Image
+                        width={250}
+                        height={250}
+                        src={`${API_SERVER_HOST}/api/view/item/${data.auctionImageList[0].fileName}`}
+                    ></Image>
                 </Col>
+
+                <Col
+                    md={8}
+                    className="p-3 text-center"
+                    style={{ marginTop: 15 }}
+                >
+                    <FormTextBox
+                        name="경매 유형"
+                        readOnly={true}
+                        value={data.auctionType}
+                    />
+                    <FormTextBox
+                        name="경매 제목"
+                        readOnly={true}
+                        value={data.title}
+                    />
+                    <FormTextBox
+                        name="시작 시간"
+                        readOnly={true}
+                        value={data.startTime}
+                    />
+                    <FormTextBox
+                        name="종료 시간"
+                        readOnly={true}
+                        value={data.endTime}
+                    />
+                    <FormTextBox
+                        name="판매자"
+                        readOnly={true}
+                        value={data.seller.name}
+                    />
+                    <FormTextBox
+                        name="설명"
+                        readOnly={true}
+                        value={data.description}
+                    />
+                </Col>
+            </Row>
+            <Row md={8} className="p-3 text-center justify-content-md-center">
+                <Form.Label
+                    style={{
+                        textAlign: "center",
+                        marginBottom: 20,
+                        marginTop: 20,
+                        fontSize: "30px",
+                        fontWeight: "bold",
+                    }}
+                >
+                    물품 정보(Item)
+                </Form.Label>
+            </Row>
+            <Row
+                style={{
+                    border: "5px solid black",
+                    borderRadius: "20px",
+                    padding: "30px",
+                }}
+            >
+                <Col
+                    md="auto"
+                    style={{ alignItems: "center", display: "flex" }}
+                >
+                    <Image
+                        width={250}
+                        height={250}
+                        src={`${API_SERVER_HOST}/api/view/item/${data.item.itemImageList[0].fileName}`}
+                    ></Image>
+                </Col>
+
                 <Col
                     md={8}
                     className="p-3 text-center"
@@ -48,33 +183,58 @@ const ProcessComponent = ({ pno }) => {
                     <FormTextBox
                         name="물품 유형"
                         readOnly={true}
-                        value="사무용품"
+                        value={data.item.type}
                     />
                     <FormTextBox
                         name="물품 명"
                         readOnly={true}
-                        value={"시바 쿠폰"}
+                        value={data.item.name}
                     />
                     <FormTextBox
-                        name="시작 금액"
+                        name="물품 설명"
                         readOnly={true}
-                        value={10000}
+                        value={data.item.description}
                     />
-                    <FormTextBox
-                        name="현재 금액"
-                        readOnly={true}
-                        value={200000}
-                    />
-                    <FormTextBox
-                        name="판매자"
-                        readOnly={true}
-                        value={"홍길동"}
-                    />
-                    <FormTextBox
-                        name="설명"
-                        readOnly={true}
-                        value={"시바견이 소중히 아끼는 쿠폰이다."}
-                    />
+                </Col>
+            </Row>
+            <Row
+                style={{
+                    marginTop: "40px",
+                    fontSize: "40px",
+                    color: "white",
+                }}
+            >
+                <Col
+                    style={{
+                        padding: "30px",
+                        marginRight: "50px",
+                        backgroundColor: "green",
+                        borderRadius: "50px",
+                        textAlign: "center",
+                    }}
+                >
+                    <span>{KRW(data.minPrice)}</span>
+                </Col>
+                <Col
+                    style={{
+                        padding: "30px",
+                        marginRight: "50px",
+                        backgroundColor: "orange",
+                        borderRadius: "50px",
+                        textAlign: "center",
+                    }}
+                >
+                    <span>{KRW(data.price)}</span>
+                </Col>
+                <Col
+                    style={{
+                        padding: "30px",
+                        backgroundColor: "red",
+                        borderRadius: "50px",
+                        textAlign: "center",
+                    }}
+                >
+                    <span>{KRW(data.minPrice)}</span>
                 </Col>
             </Row>
             <Row
